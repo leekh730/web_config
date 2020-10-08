9f6bcde71c8a165eeb60354d0bac648813e58fe8
Kunhee Lee <kunheeuk@naver.com>
2020-10-08 11:54:41 +0900

    startapp_home

--- web_config/urls.py
+++ web_config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from restapi import views as restapiview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',restapiview.home, name='home'),
 ]
